1d97c79fb7af239cc0781fd8976823d10a63aec7
karollept <karollep@metropolia.fi>
2025-08-19 13:41:39 +0300

    Päivitystä

---
+++ mod-02/teht3.py
@@ -0,0 +1,13 @@
+"Kirjoita ohjelma, joka kysyy suorakulmion kannan ja korkeuden. "
+"Ohjelma tulostaa suorakulmion piirin ja pinta-alan."
+"Suorakulmion piiri tarkoittaa sen neljän sivun yhteispituutta."
+
+kanta = input("Anna suorakulmion kannan pituus: ")
+kanta = float(kanta)
+korkeus = input("Anna suorakulmion korkeus: ")
+korkeus = float(korkeus)
+
+piiri = kanta + kanta + korkeus + korkeus
+print("Suorakulmion piiri on " + str(piiri))
+pintaala = kanta * korkeus
+print("Suorakulmion pinta-ala on " + str(pintaala))
---
+++ mod-02/teht4.py
@@ -0,0 +1,17 @@
+"Kirjoita ohjelma, joka kysyy kolme kokonaislukua. "
+"Ohjelma tulostaa lukujen summan, tulon ja keskiarvon."
+
+yksi = input("Anna ensimmäinen kokonaisluku: ")
+kaksi = input("Anna toinen kokonaisluku: ")
+kolme = input("Anna kolmas kokonaisluku: ")
+yksi = float(yksi)
+kaksi = float(kaksi)
+kolme = float(kolme)
+
+summa = yksi + kaksi + kolme
+tulo = yksi * kaksi * kolme
+keskiarvo = summa / 3
+print("Kokonaislukujen summa on " + str(summa))
+print("Kokonaislukujen tulo on " + str(tulo))
+print("Kokonaislukujen keskiarvo on " + str(keskiarvo))
+print("DONE")
