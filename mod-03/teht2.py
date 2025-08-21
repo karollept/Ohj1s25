2058da74debada4ee356b965e24038e3ff04f32d
karollept <karollep@metropolia.fi>
2025-08-21 10:43:38 +0300

    update

--- mod-03/teht2.py
+++ mod-03/teht2.py
@@ -1,7 +1,26 @@
-#Kirjoita ohjelma, joka kysyy käyttäjältä laivan hyttiluokan (LUX, A, B, C) ja tulostaa sen sanallisen kuvauksen alla olevan luettelon mukaisesti. Tehtävässä on käytettävä if/elif/else-toistorakennetta.
+#Kirjoita ohjelma, joka kysyy käyttäjältä laivan hyttiluokan (LUX, A, B, C) ja tulostaa sen sanallisen
+#kuvauksen alla olevan luettelon mukaisesti.
+#Tehtävässä on käytettävä if/elif/else-toistorakennetta.
 
 #LUX on parvekkeellinen hytti yläkannella.
 #A on ikkunallinen hytti autokannen yläpuolella.
 #B on ikkunaton hytti autokannen yläpuolella.
 #C on ikkunaton hytti autokannen alapuolella.
 #Jos käyttäjä syöttää kelvottoman hyttiluokan, ohjelma tulostaa Virheellinen hyttiluokka.
+
+hytti = input("Kerro hyttiluokkasi hyttisi kuvausta varten: ")
+
+if (hytti == "LUX" or hytti == "lux" or hytti == "Lux"):
+    print("Hyttisi LUX on parvekkeellinen hytti yläkannella.")
+
+elif (hytti == "A" or hytti == "a"):
+    print("Hyttisi A on ikkunallinen hytti autokannen yläpuolella.")
+
+elif (hytti == "B" or hytti == "b"):
+    print("Hyttisi B on ikkunaton hytti autokannen yläpuolella.")
+
+elif (hytti == "C" or hytti == "c"):
+    print("Hyttisi C on ikkunaton hytti autokannen alapuolella.")
+
+else:
+    print("Virheellinen hyttiluokka.")
--- mod-03/teht3.py
+++ mod-03/teht3.py
@@ -1,4 +1,27 @@
-#Kirjoita ohjelma, joka kysyy käyttäjän biologisen sukupuolen ja hemoglobiiniarvon (g/l). Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen, normaali vai korkea.
+#Kirjoita ohjelma, joka kysyy käyttäjän biologisen sukupuolen ja hemoglobiiniarvon (g/l).
+#Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen, normaali vai korkea.
 
 #Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
 #Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+
+biolog_s = input("Kerro biologinen sukupuolesi: ")
+hemogl_a = float(input("Kerro sinun hemoglobiiniarvo: "))
+
+if (biolog_s == "Nainen" or biolog_s == "NAINEN" or biolog_s == "nainen"):
+    if (hemogl_a >= 117 and hemogl_a <= 175):
+        print("Hemoglobiiniarvosi on normaali.")
+    elif hemogl_a > 175:
+        print("Hemoglobiiniarvosi on korkea.")
+    else:
+        print("Hemoglobiiniarvosi on alhainen.")
+
+elif (biolog_s == "Mies" or biolog_s == "MIES" or biolog_s == "mies"):
+    if (hemogl_a >= 134 and hemogl_a <= 195):
+        print("Hemoglobiiniarvosi on normaali.")
+    elif hemogl_a > 185:
+        print("Hemoglobiiniarvosi on korkea.")
+    else:
+        print("Hemoglobiiniarvosi on alhainen.")
+
+else:
+    print("En ymmärtänyt.")
