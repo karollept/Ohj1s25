56a4cd2592f8b2b53fed851bb7f4f230ea7259e6
karollept <karollep@metropolia.fi>
2025-08-28 13:02:46 +0300

    update

--- mod-04/teht4.py
+++ mod-04/teht4.py
@@ -2,3 +2,21 @@
 # Kone arvuuttelee lukua pelaajalta siihen asti, kunnes tämä arvaa oikein.
 # Kunkin arvauksen jälkeen ohjelma tulostaa tekstin Liian suuri arvaus, Liian pieni arvaus tai Oikein.
 # Huomaa, että tietokone ei saa vaihtaa lukuaan arvauskertojen välissä.
+
+import random
+
+tietokone_num = str(random.randint(1, 10))
+print(tietokone_num)
+peli_jatkuu = True
+
+while peli_jatkuu:
+    hlo_num = str(input("Arvaa tietokoneen numero: "))
+    if hlo_num == tietokone_num:
+        print("Oikein, peli loppuu.")
+        break
+    elif hlo_num > tietokone_num:
+        print("Liian suuri arvaus, arvaa uudelleen.")
+    elif hlo_num < tietokone_num:
+        print("Liian pieni arvaus, arvaa uudelleen.")
+    else:
+        print("En ymmärrä, arvaa uudelleen.")
