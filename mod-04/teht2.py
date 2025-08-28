70c7c403464ffc3489cd12dffb0d2e94b9572cae
karollept <karollep@metropolia.fi>
2025-08-28 12:26:00 +0300

    update

--- mod-04/teht2.py
+++ mod-04/teht2.py
@@ -1,2 +1,10 @@
-#Kirjoita ohjelma muuntamaan tuumia senttimetreiksi niin kauan kunnes käyttäjä antaa negatiivisen tuumamäärän.
+#Ohjelma tuumat to cm till input is negative
 # Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
+
+#1 tuuma = 2.54 cm
+tuuma = float(input("Anna tuuma "))
+while tuuma >= 0:
+    sent = tuuma * 2.54
+    print("Antamasi tuumat muutettuna sentimetreihin on " + str(sent) + "cm")
+    tuuma = float(input("Anna tuuma "))
+print("Lopetan toiminnan")
--- mod-04/teht3.py
+++ mod-04/teht3.py
@@ -1,3 +1,14 @@
 #Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
 # kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 # Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
+print("Kun haluat päättää ohjelman kirjoita ´---´")
+numeroja = tuple()
+lukuja = input("Anna minulle lukuja ")
+while int(lukuja) % 1 == 0:
+    numeroja = numeroja + (lukuja,)
+    lukuja = input("Anna minulle lukuja ")
+    if lukuja == "---":
+        break
+
+print("Isoin luku oli " + max(numeroja))
+print("Ja pienin luku oli " + min(numeroja))
