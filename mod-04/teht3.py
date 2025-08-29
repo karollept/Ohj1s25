804a168fb638b75f7fe92a64c44926c1017e9221
karollept <karollep@metropolia.fi>
2025-08-29 17:32:22 +0300

    update

--- mod-04/teht3.py
+++ mod-04/teht3.py
@@ -1,14 +1,16 @@
 #Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
 # kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
 # Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.
-print("Kun haluat päättää ohjelman kirjoita ´---´")
-numeroja = tuple()
+print("Kun haluat päättää ohjelman paina enter")
+numeroja = []
 lukuja = input("Anna minulle lukuja ")
 while int(lukuja) % 1 == 0:
-    numeroja = numeroja + (lukuja,)
+    numeroja = numeroja + [lukuja,]
     lukuja = input("Anna minulle lukuja ")
-    if lukuja == "---":
+    if lukuja == "":
         break
 
-print("Isoin luku oli " + max(numeroja))
-print("Ja pienin luku oli " + min(numeroja))
+print(numeroja)
+
+print(f"Isoin luku oli {max(numeroja)}")
+print(f"Ja pienin luku oli {min(numeroja)}")
