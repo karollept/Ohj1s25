93bc0b2bf6343fb1575e2b4811f0aab2d90daf4b
karollept <karollep@metropolia.fi>
2025-08-29 09:57:12 +0300

    update

--- mod-04/teht5.py
+++ mod-04/teht5.py
@@ -3,3 +3,25 @@
 # Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa.
 # Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
 # (Oikea käyttäjätunnus on python ja salasana rules).
+
+kauttaja = ("Mars")
+salasana = ("k4ks1&k0lm3")
+
+
+kokeiltu = 0
+mahdollisuuksia = 5
+
+while kokeiltu < mahdollisuuksia:
+    kayttaja = input("Anna käyttäjätunnus: ")
+    salesana = input("Anna salasana: ")
+    if kayttaja == kauttaja and salasana == salasana:
+        print("Tervetuloa.")
+        break
+    else:
+        print("Väärä käyttäjätunnus tai salasana")
+        kokeiltu = kokeiltu + 1
+else:
+    print("Pääsy evätty")
+
+
+
