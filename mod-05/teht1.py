3f98cdfe2d25d564fca45381b6dbc550a73b1ab3
karollept <karollep@metropolia.fi>
2025-09-03 16:47:37 +0300

    update

--- mod-05/teht1.py
+++ mod-05/teht1.py
@@ -1,3 +1,20 @@
 #Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
 #Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
 #Käytä for-toistorakennetta.
+
+import random
+luku = []
+
+
+n = int(input("Kuinka montaa arpakuutiota käytetään "))
+summa = 0
+kertaa = 0
+
+for i in range(n):
+    noppa_num = random.randint(1, 6)
+    summa += noppa_num
+    luku.append(noppa_num)
+    kertaa = kertaa + 1
+
+print(luku)
+print(summa)
