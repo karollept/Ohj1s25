2d417be4b511f0b10e2d0317c826c3fc09935d3d
karollept <karollep@metropolia.fi>
2025-09-05 18:47:36 +0300

    update

--- mod-06/teht4.py
+++ mod-06/teht4.py
@@ -1,3 +1,12 @@
 #Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
 #Ohjelma palauttaa listassa olevien lukujen summan.
 #Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
+
+kokonais = [12, 3, 4, 11, 9, 7]
+
+def lasku():
+    print(sum(kokonais))
+    return
+
+lasku()
+print("Ohjelma päättyy")
