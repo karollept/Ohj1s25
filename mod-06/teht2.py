8ec415bb469d53745e6c248c5af9c568cdb74733
karollept <karollep@metropolia.fi>
2025-09-05 15:54:35 +0300

    update

--- mod-06/teht2.py
+++ mod-06/teht2.py
@@ -2,3 +2,15 @@
 # Muokatun funktion avulla voit heitellä esimerkiksi 21-tahkoista roolipelinoppaa.
 # Edellisestä tehtävästä poiketen nopan heittelyä jatketaan pääohjelmassa kunnes saadaan nopan maksimisilmäluku,
 # joka kysytään käyttäjältä ohjelman suorituksen alussa.
+import random
+
+maksimisilmaluku = int(input("Anna nopan maksimisilmäluku "))
+def heittaa(tahkoja):
+    heitto = random.randint(1,tahkoja)
+    print(heitto)
+    while heitto != maksimisilmaluku:
+        heitto = random.randint(1,tahkoja)
+        print(heitto)
+    return
+
+heittaa(maksimisilmaluku)
