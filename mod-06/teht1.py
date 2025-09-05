f6740f5c8af2c8f5b380ee39dda91267b89dda42
karollept <karollep@metropolia.fi>
2025-09-05 15:09:53 +0300

    update

--- mod-06/teht1.py
+++ mod-06/teht1.py
@@ -1,6 +1,7 @@
 #Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6.
 # Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen.
 # Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
+import random
 
 def heittaa():
     heitto = random.randint(1,6)
