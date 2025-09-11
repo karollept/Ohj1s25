a44a55413433eb4e3ce0a8396d64317542a47122
karollept <karollep@metropolia.fi>
2025-09-11 11:54:38 +0300

    update

--- mod-08/teht1.py
+++ mod-08/teht1.py
@@ -2,3 +2,27 @@
 # Ohjelma hakee ja tulostaa koodia vastaavan lentokentän nimen ja sen
 # sijaintikunnan kurssilla käytettävästä lentokenttätietokannasta.
 # ICAO-koodi on tallennettuna airport-taulun ident-sarakkeeseen.
+
+import mysql.connector
+
+def lentokentät(icao):
+    sql = f"SELECT name, municipality FROM airport WHERE ident =%s"
+    kursori = yhteys.cursor(dictionary=True)
+    kursori.execute(sql, (icao,))
+    tulos = kursori.fetchall()
+    return tulos
+
+koodi = input("syötä lentoaseman ICAO-koodi: ")
+kentat = lentokentät(koodi)
+for kentta in kentat:
+    print(f"Nimi: {kentta['name']}, ICAO: {kentta['municipality']}")
+
+
+yhteys = mysql.connector.connect(
+    host='localhost',
+    port= 3306,
+    database='flight_game',
+    user='kartsu',
+    password='black_rose',
+    autocommit=True
+    )
