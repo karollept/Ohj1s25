c0ef7c30b690be43785fd7e411f9e8b1e3da4b72
karollept <karollep@metropolia.fi>
2025-09-11 10:41:49 +0300

    update

--- mod-07/teht3.py
+++ mod-07/teht3.py
@@ -7,3 +7,43 @@
 #(ICAO-koodi on lentoaseman yksilöivä tunniste.
 #Esimerkiksi Helsinki-Vantaan lentoaseman ICAO-koodi on EFHK.
 #Löydät koodeja helposti selaimen avulla.)
+
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host='localhost',
+    port= 3306,
+    database='flight_game',
+    user='kartsu',
+    password='black_rose',
+    autocommit=True
+    )
+
+def lentokentät():
+    sql = f"SELECT * FROM airport"
+    kursori = yhteys.cursor(dictionary=True)
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
+
+def haeLentokentta(koodi):
+    sql = f"SELECT * FROM airport WHERE ident='{koodi}'"
+    kursori = yhteys.cursor(dictionary=True)
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
+
+tehdaan = input("Mitä haluat tehdä, haluatko 1. syöttää uuden lentoaseman, 2. hakea jo syötetyn lentoaseman tiedot vai 3. lopettaa ")
+while tehdaan != "3":
+    if tehdaan == "1":
+        koodi = input("syötä uuden lentoaseman ICAO-koodi: ")
+        nimi = input("syötä uuden lentoaseman nimi: ")
+        lentokentät[koodi] = nimi
+
+    else:
+        koodi2 = input("syötä lentoaseman ICAO-koodi: ")
+        kentat = haeLentokentta(koodi2)
+        for kentta in kentat:
+            print(f"Nimi: {kentta['name']}, ICAO: {kentta['ident']}")
+    tehdaan = input(
+        "Mitä haluat tehdä, haluatko 1. syöttää uuden lentoaseman, 2. hakea jo syötetyn lentoaseman tiedot vai 3. lopettaa ")
